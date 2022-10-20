e5b112056a624c8b25099f07dd48149bb8ecda8d
Louis Rabeno <lrabeno@gmail.com>
2022-10-19 22:22:53 -0400

    express routes set up

--- server/api/album.js
+++ server/api/album.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
-const { Album } = require("../db");
+const { Album, Artist, Song } = require("../db");
 
 router.get("/", async (req, res, next) => {
   try {
-    const albums = await Album.findAll();
+    const albums = await Album.findAll({
+      include: Artist,
+    });
     res.send(albums);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const singleAlbum = await Album.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [Artist, Song],
+    });
+    res.send(singleAlbum);
   } catch (error) {
     next(error);
   }
