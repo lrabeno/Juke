7b59fe652dc36fafcb72a45b4b7c298d8ad47ac6
Louis Rabeno <lrabeno@gmail.com>
2022-10-19 21:08:00 -0400

    commit before upstream

---
+++ server/api/album.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { Album } = require("../db");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const albums = await Album.findAll();
+    res.send(albums);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,7 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 // connect your API routes here!
+const albumRouter = require("./album");
+router.use("/albums", albumRouter);
 
-module.exports = router
+module.exports = router;
